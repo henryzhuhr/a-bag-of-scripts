a467efcd89f1aa350c811797c6e33a0861a161ea
henryzhuhr <henry.zhuhr@icloud.com>
2025-05-12 00:07:59 +0800

    feat(scripts): 重命名全景照片脚本
    - 修改脚本名称为中文，解决编码问题
    - 更新脚本路径以处理全景照片
    - 添加命令行参数解析，支持确认操作
    - 优化重命名逻辑，排除目录和系统文件
    - 增加交互式确认步骤，提高安全性

--- scripts/全景照片重命名.py
+++ scripts/全景照片重命名.py
@@ -3,6 +3,7 @@
 
 """
 
+import argparse
 import os
 import typing
 from genericpath import exists
@@ -14,30 +15,39 @@ from pkg._types.photo import FileTag
 from utils.xphoto import XPhoto
 
 # ================== 目录路径设置 ==================
-BD = PhotographDir.ICLOUD_PHOTO_LOCAL
+BD = PhotographDir.ICLOUD_PANO_LOCAL
 """基本目录 base dir"""
-
-BD = f"{BD}/250501-【旅游】长沙/250501-DCIMmxhnight/PANORAMA"
 
 
 class Args:
     PANO_DIR_LIST: typing.List[FileTag] = [
         # fmt: off
-        # FileTag(tag="梅溪湖艺术中心大剧院", dir=f"{BD}/001_0016"),
+        # FileTag(tag="xxx", dir=f"{BD}/001_0016"),
         # fmt: on
         # ============ 目录范围 ============
         # fmt: off
         *[
-            FileTag(tag="梅溪湖艺术中心大剧院", dir=f"{BD}/001_00{index:02d}")
+            FileTag(tag="xxx", dir=f"{BD}/001_00{index:02d}")
             for index in range(0, 100 + 1)  # 左闭右开，所以右边+1
         ]
         # fmt: on
     ]
     """全景照片目录"""
 
+    def __init__(self) -> None:
+        args = self.get_args()
+        self.execute_confirm: bool = args.yes
+
+    @staticmethod
+    def get_args():
+        parse = argparse.ArgumentParser(description="读取相机exif重命名文件")
+        parse.add_argument("-y", "--yes", action="store_true", help="是否确认重命名")
+        return parse.parse_args()
+
 
 def main():
     print("Hello from common-scripts!")
+    args = Args()
 
     for photo_dir in Args.PANO_DIR_LIST:
         print(f"{photo_dir.tag} - {photo_dir.dir}")
@@ -50,6 +60,9 @@ def main():
         pano_photos: typing.List[XPhoto] = []
 
         for file in os.listdir(photo_dir.dir):
+            # 排除目录
+            if not os.path.isfile(os.path.join(photo_dir.dir, file)):
+                continue
             # 排除 macOS 系统文件
             if file.startswith(".DS_Store"):
                 continue
@@ -69,7 +82,6 @@ def main():
         # 在 photo_dir.dir 同目录下创建 YYMMDD-<photo_dir.tag>-HHMMSS 文件夹，然后将全部文件以 YYMMDD-<photo_dir.tag>-HHMMSS_<index> 重命名
         # 1. 创建同目录下的新文件夹
         date_time = pano_photos[0].photo_info.exif_data.date_time_original
-        print(type(date_time), date_time)
 
         dir_name = f"{date_time.strftime('%Y%m%d')[2:]}-{photo_dir.tag}_{date_time.strftime('%H%M%S')}"
         dir_path = os.path.join(os.path.dirname(photo_dir.dir), dir_name)
@@ -78,12 +90,48 @@ def main():
 
         os.makedirs(dir_path, exist_ok=True)
 
+        task_list = []
         for index, xphoto in enumerate(pano_photos):
             new_file_name = f"{dir_name}_{index:02d}{os.path.splitext(xphoto.photo_info.file_path)[1]}"
 
             new_file_path = os.path.join(dir_path, new_file_name)
             logger.info(f"重命名: {xphoto.photo_info.file_path} -> {new_file_path}")
-            os.rename(xphoto.photo_info.file_path, new_file_path)
+            task_list.append((xphoto.photo_info.file_path, new_file_path))
+
+        if not args.execute_confirm:
+            run_task(task_list)
+        else:
+            incorrect_input_str: str = None
+
+            execute_cnt = 0
+            while True:
+                execute_cnt += 1
+                if execute_cnt > 5:
+                    logger.error("错误执行次数过多，已取消执行")
+                    break
+                tip_info = (
+                    str(
+                        f'未知输入 "{incorrect_input_str}" '
+                        if incorrect_input_str
+                        else "是否执行以上的重命名操作"
+                    )
+                    + "，确认执行输入(yes/no):"
+                )
+                confirm = input(tip_info).lower()
+                if confirm in ["yes", "y"]:
+                    run_task(task_list)
+                    break
+                elif confirm in ["no", "n"]:
+                    logger.info("已取消执行")
+                    break
+                else:
+                    incorrect_input_str = confirm
+                continue
+
+
+def run_task(task_list: typing.List[typing.Tuple[str, str]]):
+    for src, dst in task_list:
+        os.rename(src, dst)
 
 
 if __name__ == "__main__":
